5f0f5bc3d8cc6488f033169fd10dda8024755266
bestswt <K23065445@kcl.ac.uk>
2024-07-08 22:37:50 +0100

    1

--- check_param.py
+++ check_param.py
@@ -1,22 +1,15 @@
-from nnunetv2.inference.predict import load_model_and_checkpoint_files
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from torchinfo import summary
-import torch
 
-# 指定模型文件路径
-model_file_path = ("/scratch/users/k23065445/baseline/nnUNet_results/Dataset277_TotalSegmentator"
-                   "/nnUNetTrainerNoMirroring__nnUNetPlans__3d_fullres/fold_0/checkpoint_best.pth")
+# 创建并初始化模型
+model = nnUNetTrainer(
+    plans="/scratch/users/k23065445/baseline/nnUNet_preprocessed/Dataset277_TotalSegmentator/nnUNetPlans.json",
+    configuration="3d_fullres",
+    fold=277,
+    dataset_json="/scratch/users/k23065445/baseline/nnUNet_preprocessed/Dataset277_TotalSegmentator/dataset.json",
+)
+model.initialize()
 
-# 加载模型和检查点文件
-trainer, params = load_model_and_checkpoint_files(model_file_path)
-
-# 获取模型
-model = trainer.network
-
-# 方法1：使用torchsummary
-model_summary = summary(model, input_size=(2, 112, 112, 128))  # 这里的input_size应根据您的实际输入大小进行调整
-# print(f"Total FLOPs: {model_summary.total_ops}")
-
-
-# 方法2：直接计算参数数量
-# total_params = sum(p.numel() for p in model.parameters())
-# print(f"Total number of parameters: {total_params}")
+# 使用随机输入数据来计算参数量和 FLOPs
+input_size = (2, 1, 128, 128, 128)  # 根据你的输入数据尺寸进行调整
+summary(model.network, input_size=input_size)
