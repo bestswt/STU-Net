c78f6797370a55272c4edb6dfb604b08dd78d069
bestswt <K23065445@kcl.ac.uk>
2024-07-09 12:30:47 +0100

    1

--- check_param.py
+++ check_param.py
@@ -1,4 +1,4 @@
-from nnunetv2.training.nnUNetTrainer.STUNetTrainer import STUNetTrainer_huge as nnUNetTrainer
+from nnunetv2.training.nnUNetTrainer.STUNetTrainer import STUNetTrainer_small as nnUNetTrainer
 from torchinfo import summary
 import json
 from fvcore.nn import FlopCountAnalysis
