9716a35c76899ec6bae38b00f2dd754e1066bbd7
bestswt <K23065445@kcl.ac.uk>
2024-07-09 12:30:31 +0100

    1

--- check_param.py
+++ check_param.py
@@ -1,6 +1,9 @@
 from nnunetv2.training.nnUNetTrainer.STUNetTrainer import STUNetTrainer_huge as nnUNetTrainer
 from torchinfo import summary
 import json
+from fvcore.nn import FlopCountAnalysis
+import torch
+
 
 # 读取并解析 dataset_json 文件
 with open("/scratch/users/k23065445/baseline/nnUNet_preprocessed/Dataset277_TotalSegmentator/dataset.json", 'r') as f:
@@ -20,5 +23,10 @@ model = nnUNetTrainer(
 model.initialize()
 
 # 使用随机输入数据来计算参数量和 FLOPs
-input_size = (1, 1, 112, 112, 128)  # 根据你的输入数据尺寸进行调整
+input_size = (2, 1, 112, 112, 128)  # 根据你的输入数据尺寸进行调整
 summary(model.network, input_size=input_size)
+
+# 计算 FLOPs
+random_input = torch.randn(input_size)
+flops = FlopCountAnalysis(model.network, random_input)
+print(f"FLOPs: {flops.total()/1e9:.1f} G")
